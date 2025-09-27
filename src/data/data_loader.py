d63ea296774d41fedb52307ed59ee05e060e1a47
Elliott <elliottyankelevich@gmail.com>
2025-09-27 19:31:07 -0400

    Added docstring explaining season assignment function for better legibility

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def assign_proper_seasons(month: int) -> str:
+    """Assign seasons based on month"""
     if month in [12, 1, 2]:
         return 'Winter'
     elif month in [3, 4, 5]:
